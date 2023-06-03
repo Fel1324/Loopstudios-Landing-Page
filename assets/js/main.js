11f187a9eb40b390a4eb3430045b40de1e423803
Rafael R. de Oliveira <rafael.roberto200618@gmail.com>
2023-06-03 16:19:37 -0300

    finish main and start footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,52 @@ const logo = 'assets/img/logo.svg';
 const menu = 'assets/img/icon-hamburger.svg';
 const introductionImg = 'assets/img/mobile/image-interactive.jpg';
 
-const objs = [
+// GENERIC FUNCTIONS
+function createImg(className, img, alt){
+    return `<img class="${className}" src="${img}" alt="${alt}">`;
+}
+// -----------------------
+
+// HEADER
+function createHeaderContent(logo, menu){
+    return `
+        <div>
+            ${createImg('logo', logo, 'logo da loopstudios')}
+
+            <button class="header__button button-open">
+                ${createImg('', menu, 'Botão que abri o menu lateral')}
+            </button>    
+        </div>
+
+        <div>
+            <h1 class="header__title">Immersive experiences that deliver</h1>
+        </div>
+    `;
+}
+// -----------------------
+
+// MAIN
+// PRIMARY SECTION
+function createSectionIntroduction(source){
+    return `
+        <section class="main__introduction">
+            ${createImg('introduction__img', source, '')}
+
+            <h2 class="introduction__title title">The leader in interactive VR</h2>
+
+            <p class="introduction__paragraph">
+                Founded in 2011, Loopstudios has been producing world-class virtual reality 
+                projects for some of the best companies around the globe. Our award-winning 
+                creations have transformed businesses through digital experiences that bind 
+                to their brand.
+            </p>
+        </section>
+    `;
+}
+
+// SECONDARY SECTION
+// CREATE ITEMS
+const itemsCreations = [
     {
         imgSrc: 'assets/img/mobile/image-deep-earth.jpg',
         title: 'Deep <br> earth'
@@ -46,71 +91,24 @@ const objs = [
         imgSrc: 'assets/img/mobile/image-fisheye.jpg',
         title: 'Make it <br> fisheye'
     }
-]
+];
 
-// GENERIC FUNCTIONS
-function createImg(className, img, alt){
-    return `<img class="${className}" src="${img}" alt="${alt}">`;
-}
-
-function createSubTitle(className, content){
-    return `<h3 class="${className}">${content}</h3>`;
-}
-// -----------------------
-
-// HEADER
-function createHeaderContent(logo, menu){
-    return `
-        <div>
-            ${createImg('header__logo', logo, 'logo da loopstudios')}
-
-            <button class="header__button button-open">
-                ${createImg('', menu, 'Botão que abri o menu lateral')}
-            </button>    
-        </div>
-
-        <div>
-            <h1 class="header__title">Immersive experiences that deliver</h1>
-        </div>
-    `;
-}
-// -----------------------
-
-// MAIN
-// PRIMARY SECTION
-function createSectionIntroduction(source){
-    return `
-        <section class="main__introduction">
-            ${createImg('introduction__img', source, '')}
-
-            <h2 class="introduction__title title">The leader in interactive VR</h2>
-
-            <p class="introduction__paragraph">
-                Founded in 2011, Loopstudios has been producing world-class virtual reality 
-                projects for some of the best companies around the globe. Our award-winning 
-                creations have transformed businesses through digital experiences that bind 
-                to their brand.
-            </p>
-        </section>
-    `
-}
-
-// SECONDARY SECTION
-// CREATE CARDS
-function createCardsCreations(objs){
+function createItemsCreations(objs){
     let objValue = '';
 
     objs.forEach(obj => {
         objValue += `
-            <div class="creations__cards">
-                <img class="creations__img" src="${obj.imgSrc}" alt="${obj.title}">
-                <h3 class="creations__subtitle">${obj.title}</h3>
-            </div>
+            <li class="creations__item">
+                <a href="#!" class="creations__link">
+                    <h3 class="creations__item-title">${obj.title}</h3>
+                    <img class="creations__item-img" src="${obj.imgSrc}" alt="${obj.title}">
+                </a>
+            </li>
         `
     }); 
 
     const htmlObjValue = 0;
-    return htmlObjValue.innerHTML = `${objValue}`;
+    return htmlObjValue.innerHTML = objValue;
 }
 
 // CREATE SECONDARY SECTION
@@ -119,13 +117,13 @@ function createSectionCreations(){
         <section class="main__creations">
             <h2 class="creations__title title">Our creations</h2>
 
-            <div id="creationsC" class="creations__container">
-                ${createCardsCreations(objs)}
-            </div>
+            <ul class="creations__list">
+                ${createItemsCreations(itemsCreations)}
+            </ul>
 
             <button class="creations__button" aria-label="Ver todos os items">See all</button>
         </section>
-    `
+    `;
 }
 
 function createMainContent(){
@@ -133,5 +131,47 @@ function createMainContent(){
 }
 // -----------------------
 
+// FOOTER
+// CREATE LINKS
+const linksFooter = [
+    'About',
+    'Careers',
+    'Events',
+    'Products',
+    'Support'
+];
+
+function createLinksFooter(links){
+    let linkValue = '';
+
+    links.forEach(link => {
+        linkValue += `
+            <li class="footer__item">
+                <a href="#!" class="footer__link">${link}</a>
+            </li>
+        `
+    });
+
+    const htmlLinkValue = 0;
+    return htmlLinkValue.innerHTML = linkValue;
+}
+
+function createFooterContent(logo){
+    return `
+        <div class="footer__col-a">
+            ${createImg('logo', logo, 'logo da loopstudios')}
+
+            <ul class="footer__list">
+                ${createLinksFooter(linksFooter)}
+            </ul>
+        </div>
+
+        <div class="footer__col-b">
+            <p class="footer__copy">© 2021 Loopstudios. All rights reserved.</p>
+        </div>
+    `
+}
+
 headerContent.innerHTML = createHeaderContent(logo, menu);
 mainContent.innerHTML = createMainContent();
+footerContent.innerHTML = createFooterContent(logo);
